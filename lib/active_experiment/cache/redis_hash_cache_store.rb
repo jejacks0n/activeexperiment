41fd7cfda4553205860404df49a3faef468ef1e0
Jeremy Jackson <jejacks0n@gmail.com>
2022-12-09 20:50:54 -0700

    Adds an initial redis hash cache store

--- lib/active_experiment/cache/redis_hash_cache_store.rb
+++ lib/active_experiment/cache/redis_hash_cache_store.rb
@@ -14,8 +14,8 @@ module ActiveExperiment
     # optimized way that redis stores hashes.
     #
     # The data structure:
-    #   key: experiment.name
-    #   fields: run key => variant name
+    #   key: experiment name
+    #   fields: key => entry
     #
     # To use this cache in an experiment:
     #
@@ -26,6 +26,43 @@ module ActiveExperiment
     #     use_cache_store :redis_hash
     #   end
     class RedisHashCacheStore < ActiveSupport::Cache::RedisCacheStore
+      def length(hkey = nil)
+        if hkey
+          failsafe :read_hlen do
+            redis.then { |c| c.hlen(hkey) }
+          end
+        else
+          failsafe :read_dbsize do
+            redis.then { |c| c.dbsize }
+          end
+        end
+      end
+
+      private
+        def hkey(key)
+          parts = key.to_s.split(":")
+          run_key = parts.pop
+          [Array(parts).join(":"), run_key]
+        end
+
+        def read_serialized_entry(key, raw: false, **options)
+          failsafe :read_entry do
+            redis.then { |c| c.hget(*hkey(key)) }
+          end
+        end
+
+        def write_serialized_entry(key, payload, raw: false, unless_exist: false, expires_in: nil, race_condition_ttl: nil, pipeline: nil, **options)
+          # TODO: Support pipeline?
+          failsafe :write_entry, returning: false do
+            redis.then { |c| c.hset(*hkey(key), payload) }
+          end
+        end
+
+        def delete_entry(key, **options)
+          failsafe :delete_entry, returning: false do
+            redis.then { |c| c.hdel(*hkey(key)) }
+          end
+        end
     end
   end
 end
--- test/cases/cache/redis_hash_cache_store_test.rb
+++ test/cases/cache/redis_hash_cache_store_test.rb
@@ -14,7 +14,135 @@ class RedisHashCacheStoreTest < ActiveSupport::TestCase
     skip("Skipping because redis is not available")
   end
 
-  test "this cache store" do
-    skip("implement me")
+  test "clearing the entire cache for all experiments" do
+    experiment = SubjectExperiment.new
+    experiment.run
+
+    assert_equal 1, experiment.cache_store.length
+
+    experiment.cache_store.clear
+
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "clearing the cache for a specific experiment" do
+    experiment = SubjectExperiment.new
+    experiment.run
+
+    assert_equal 1, experiment.cache_store.length
+
+    experiment.cache_store.delete_matched(experiment.name)
+
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "clearing the cache for a specific experiment that has a namespace" do
+    NamespacedCacheStoreExperiment = Class.new(SubjectExperiment) do
+      use_cache_store :redis_hash, namespace: "_exp_"
+    end
+
+    experiment = NamespacedCacheStoreExperiment.new
+    experiment.run
+
+    assert_equal 1, experiment.cache_store.length
+
+    experiment.cache_store.delete_matched(experiment.name)
+
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "caching resolved variants" do
+    experiment = SubjectExperiment.new(id: 1)
+
+    assert_equal "red", experiment.run
+    assert_equal :red, experiment.cache_store.read(experiment.cache_key)
+    assert_equal 1, experiment.cache_store.length
+
+    SubjectExperiment.run(id: 2)
+    SubjectExperiment.run(id: 3)
+
+    assert_equal 1, experiment.cache_store.length
+    assert_equal 3, experiment.cache_store.length(experiment.cache_key_prefix)
+  end
+
+  test "caching assigned variants" do
+    experiment = SubjectExperiment.new
+    experiment.set(variant: :blue)
+
+    assert_equal "blue", experiment.run
+    assert_equal :blue, experiment.cache_store.read(experiment.cache_key)
+  end
+
+  test "caching variants when segmented" do
+    experiment = SubjectExperiment.new(segment_into_green: true)
+
+    assert_equal "green", experiment.run
+    assert_equal :green, experiment.cache_store.read(experiment.cache_key)
+  end
+
+  test "skipped experiments" do
+    experiment = SubjectExperiment.new
+    experiment.skip
+
+    assert_nil experiment.run
+    assert_nil experiment.cache_store.read(experiment.cache_key)
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "skipped experiments (through inactive rollout)" do
+    InactiveExperiment = Class.new(SubjectExperiment) do
+      use_rollout :inactive
+      use_cache_store :redis_hash
+    end
+
+    experiment = InactiveExperiment.new
+    experiment.set(variant: :blue)
+
+    assert_equal "blue", experiment.run
+    assert_nil experiment.cache_store.read(experiment.cache_key)
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "skipped experiments with an assigned variant" do
+    experiment = SubjectExperiment.new
+    experiment.set(variant: :blue)
+    experiment.skip
+
+    assert_equal "blue", experiment.run
+    assert_nil experiment.cache_store.read(experiment.cache_key)
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "deleting a single entry" do
+    experiment = SubjectExperiment.new(id: 1)
+    experiment.run
+
+    assert_equal 1, experiment.cache_store.length
+
+    experiment.cache_store.delete(experiment.cache_key)
+
+    assert_equal 0, experiment.cache_store.length
+  end
+
+  test "when a cache key already exists" do
+    SubjectExperiment.set(variant: :green).run(id: 1)
+
+    assert_equal 1, SubjectExperiment.cache_store.length
+
+    experiment = SubjectExperiment.new(id: 1)
+
+    assert_equal "green", experiment.run
+    assert_equal 1, experiment.cache_store.length
+    assert_equal 1, experiment.cache_store.length(experiment.cache_key_prefix)
+  end
+
+  class SubjectExperiment < ActiveExperiment::Base
+    variant(:red) { "red" }
+    variant(:blue) { "blue" }
+    variant(:green) { "green" }
+
+    segment(into: :green) { context[:segment_into_green] }
+
+    use_cache_store :redis_hash
   end
 end
