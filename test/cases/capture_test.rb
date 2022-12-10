ed16f1ce0d809f6fa0ed8d7b89295fc80b55feac
Jeremy Jackson <jejacks0n@gmail.com>
2022-12-09 17:38:47 -0700

    Improves coverage for capturing

--- test/cases/capture_test.rb
+++ test/cases/capture_test.rb
@@ -16,6 +16,23 @@ class CaptureTest < ActiveSupport::TestCase
     assert_equal "<div><span>red</span></div>", result
   end
 
+  test "trying to capture when not capturable" do
+    UncapturableExperiment = Class.new(SubjectExperiment) do
+      def capturable?
+        false
+      end
+    end
+
+    result = UncapturableExperiment.set(capture: self).run do |experiment|
+      "<div>" +
+        experiment.on(:red) { "<span>red</span>" } +
+        experiment.on(:blue) { "<span>blue</span>" } +
+      "</div>"
+    end
+
+    assert_equal "<span>red</span>", result
+  end
+
   class SubjectExperiment < ActiveExperiment::Base
     include ActiveExperiment::Capturable
 
